46415849e52488c34f3b530f5c8697e6b5650fd7
Paul <ritzman.paul@gmail.com>
2021-03-25 08:51:53 -0700

    merged from origin, repushing

--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Switch, Route, Link } from "react-router-dom"
 import TestComponentList from "./TestComponentList"
-import PetList from "./PetList"
+// import PetList from "./PetList"
 // import AnimalIndexPage from "./AnimalIndexPage"
 // import NewAdoptionForm from "./NewAdoptionForm"
 import AnimalSurrenderForm from "./AnimalSurrenderForm"
@@ -25,15 +25,15 @@ const NavBar = props => {
         <Link to="/surrender">List Pet for Adoption</Link>
       </div>
       <div className="navbar">
-        <Link to="/adoptions">Adopt A Pet</Link>
+        <Link to="/adopt">Adopt A Pet</Link>
       </div>
       <Switch>
-        <Route exact path="/" component={TestComponentList} />
-        <Route exact path="/pets" component={PetList} />
+        {/* <Route exact path="/" component={TestComponentList} /> */}
+        {/* <Route exact path="/pets" component={PetList} /> */}
         {/* <Route exact path="/types" component={TypeList} /> */}
         {/* <Route exact path="/regions" component={RegionList} /> */}
         {/* <Route exact path="/pets/:id" component={AnimalIndexPage}> */}
-        <Route exact path="/adoptions" component={NewAdoptionForm} />
+        <Route exact path="/adopt" component={NewAdoptionForm} />
         <Route exact path="/surrender" component={AnimalSurrenderForm} />
       </Switch>
       <div className="navbar">
