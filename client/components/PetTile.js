ced3b07c2aa4761fe96e78cf53f3d3c03d98a760
a-moairu <61666763+a-moairu@users.noreply.github.com>
2021-03-25 10:47:37 -0700

    Pet list to have props and directs to PetList

--- client/components/PetTile.js
+++ client/components/PetTile.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Link } from "react-router-dom"
 
-const PetTile = () => {
+const PetTile = ({ imageUrl, name, description }) => {
   return (
     <div>
-      
+      <h3> {name} </h3>
+      <p> {description} </p>
+      <p> {imageUrl} </p>
     </div>
+    <Link to={`/pets/${id}`}> {name}} <input className="button" type="submit" value="More Info"/> </Link>
   )
 }
 
