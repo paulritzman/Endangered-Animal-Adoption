d59b47638ac580d71a497b28e59d42579dfcca79
Paul <ritzman.paul@gmail.com>
2021-03-25 12:12:29 -0700

    worked out routing error

--- client/components/App.js
+++ client/components/App.js
@@ -3,12 +3,16 @@ import { hot } from "react-hot-loader/root"
 import { Route, Switch, BrowserRouter } from "react-router-dom"
 import "regenerator-runtime/runtime"
 import NavBar from "./NavBar"
+import PetShow from "./PetShow"
+import NewAdoptionForm from "./NewAdoptionForm"
+import AnimalSurrenderForm from "./AnimalSurrenderForm"
 
 const App = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/pets" component={NavBar} />
+        <Route exact path="/pets/:id" component={PetShow} />
         <Route exact path="/adopt" component={NewAdoptionForm} />
         <Route exact path="/surrender" component={AnimalSurrenderForm} />
       </Switch>
