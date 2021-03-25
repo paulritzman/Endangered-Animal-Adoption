f7c28aa4cbdc3e144b63606beaf15934047fbdaf
a-moairu <61666763+a-moairu@users.noreply.github.com>
2021-03-24 23:05:45 -0700

    Create Nav Bar with potential working Routes and Links

---
+++ client/components/NavBar.js
@@ -0,0 +1,41 @@
+import React from "react"
+import { Switch, Route, Link } from "react-router-dom"
+import TestComponentList from "./TestComponentList"
+// import PetList from "./PetList"
+// import AnimalIndexPage from "./AnimalIndexPage"
+import NewAdoptionForm from "./NewAdoptionForm"
+import AnimalSurrenderForm from "./AnimalSurrenderForm"
+
+const NavBar = props => {
+  return (
+    <div className="row column">
+      <div className="navbar">
+        <Link to="/">Home</Link>
+      </div>
+      <div className="navbar">
+        <Link to="/pets">Animal</Link>
+      </div>
+      <div className="navbar">
+        <Link to="/petType">Types</Link>
+      </div>
+      <div className="navbar">
+        <Link to="/surrender">List Pet for Adoption</Link>
+      </div>
+      <div className="navbar">
+        <Link to="/adoptions">Adopt A Pet</Link>
+      </div>
+      <Switch>
+        <Route exact path="/" component={TestComponentList} />
+        {/* <Route exact path="/pets" component={PetList} /> */}
+        {/* <Route exact path="/pets/:id" component={AnimalIndexPage}> */}
+        <Route exact path="/adoptions" component={NewAdoptionForm} />
+        <Route exact path="/surrender" component={AnimalSurrenderForm} />
+      </Switch>
+      <div className="navbar">
+        <p>Hello Footer</p>
+      </div>
+    </div>
+  )
+}
+
+export default NavBar
