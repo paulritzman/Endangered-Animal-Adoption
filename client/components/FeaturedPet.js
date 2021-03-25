9df8d0cd4308d100d8d84830f66bce6838427564
a-moairu <61666763+a-moairu@users.noreply.github.com>
2021-03-25 13:47:22 -0700

    Added to FeaturedPet.js with a reroute to /pets

--- client/components/FeaturedPet.js
+++ client/components/FeaturedPet.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
-const FeaturedPet = () => {
+const FeaturedPet = (props) => {
+  const {id, name, description, imageUrl} = props.featured
   return (
     <div>
-      
+        <h3>{name}</h3>
+        <p>{description}</p>
+        <img src={imageUrl} />
+      <Link to={`/pets`}><input className="button" type="submit" value="More Info"/></Link>
     </div>
   )
 }
