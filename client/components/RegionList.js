fd6e6cff8bc6b5344b2312942fc7e3ab41258076
BreeBot <bree.merriweather@gmail.com>
2021-03-25 11:34:21 -0700

    created intial regionList

--- client/components/RegionList.js
+++ client/components/RegionList.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useState, useEffect } from "react"
+import fetchData from "../scripts/fetchData.js"
 
-const RegionList = () => {
+
+const RegionList = (props) => {
+  const [regions, setRegions] = useState([])
+  const [selectedRegion, setSelectedRegion] = useState(null)
+
+  useEffect(() => {
+    fetchRegions
+  }, [])
+
+  const fetchRegions = async (id) => {
+    const data = await fetchData("/api/v1/pets")
+    setRegions(id)
+  }
+
+  const changeRegions = (id) => {
+    const currentRegion = id === selectedRegion ? null : id
+    setSelectedRegion(currentRegion)
+  }
+
+  const regionListItems = regions.map((region) => {
+    let selected
+    if (selectedRegion === region.id) {
+      selected = true
+    }
+
+    const handleClick = () => {
+      changeRegion(region.id)
+    }
+
+    return (
+      <Question
+        key={region.id}
+        region={region.region}
+        selected={selected}
+        handleClick={handleClick}
+      />
+    )
+  })
+
   return (
-    <div>
-      
+    <div className="page">
+
+      <div className="region-list">
+        <input className = "button" type = "submit" value = {regionListItems}/>
+        </div>
     </div>
   )
 }
